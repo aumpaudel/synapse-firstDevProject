1793232e0b97db58abac6cbb4a55913ea62a1f75
Aum Paudel <paudelaum@gmail.com>
2026-06-02 21:13:42 +0530

    learned get vs push,querry,path parameter & request body

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI 
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    price: float 
+    is_available: bool = True
+
 app = FastAPI()
+
 @app.get("/")
 def home():
     return {"message": "Hello World"}
+
 @app.get("/about")
 def about():
     return {"message": "This is about page"}
+
+@app.get("/user/{id}")
+def user(id: int):
+    return {"user_id": id}
+
+@app.get("/search")
+def search(q: str, page:int = 1, limit: int = 10):
+    return {"query": q, "page": page, "limit": limit}
+
+@app.post("/submit")
+def create_item(item: Item):
+    return {"received": item}
